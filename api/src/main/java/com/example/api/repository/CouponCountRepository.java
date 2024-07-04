927533bd58374fa67f387c741781c7e7cd968af4
kmkwk <cheery7272@naver.com>
2024-07-04 18:02:50 +0900

    reset

---
+++ api/src/main/java/com/example/api/repository/CouponCountRepository.java
@@ -0,0 +1,9 @@
+package com.example.api.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CouponCountRepository {
+
+//    private final Re
+}
---
+++ api/src/test/java/com/example/api/service/ApplyServiceTest.java
@@ -0,0 +1,58 @@
+package com.example.api.service;
+
+import com.example.api.repository.CouponRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ApplyServiceTest {
+
+
+    @Autowired
+    private ApplyService applyService;
+
+    @Autowired
+    private CouponRepository couponRepository;
+
+    @Test
+    public void 한번만응모() {
+        applyService.apply(1L);
+        couponRepository.count();
+    }
+
+
+    @Test
+    public void 여러명응모() throws InterruptedException {
+        int threadCount = 1000;
+        ExecutorService executorService = Executors.newFixedThreadPool(32);
+        CountDownLatch latch = new CountDownLatch(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            long userId = i;
+
+            executorService.submit(() -> {
+                applyService.apply(userId);
+                try {
+                    applyService.apply(userId);
+                } finally {
+                    latch.countDown();
+                }
+            });
+        }
+
+        latch.await();
+
+        long count = couponRepository.count();
+
+        assertThat(count).isEqualTo(100);
+
+    }
+}
