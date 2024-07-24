02f329d1339bcf456b77a1d7651ea9262f8cc9d6
kmkwk <cheery7272@naver.com>
2024-07-24 19:20:07 +0900

    Spring Cloud

---
+++ apigateway-service/src/main/java/com/example/apigatewayservice/config/FilterConfig.java
@@ -0,0 +1,20 @@
+package com.example.apigatewayservice.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+public class FilterConfig {
+
+//    @Bean
+    public RouteLocator gatewayRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/users/**")
+                        .filters(f -> f.addRequestHeader("user-request","user-request-header")
+                                        .addResponseHeader("user-response","user-response-header"))
+                        .uri("http://localhost:8081"))
+                .build();
+    }
+}
